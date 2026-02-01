4726464459f483e13d07ba88b7ec79e1783c25ae
Jose David Colmenarez Gil <jcolmenarezgil@byupathway.edu>
2026-01-31 23:20:37 -0400

    fix routes.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,22 +1,8 @@
-// import postgres from 'postgres';
+import { NextResponse } from 'next/server';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
-
-// export async function GET() {
-//   try {
-//   	return Response.json(await listInvoices());
-//   } catch (error) {
-//   	return Response.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  return NextResponse.json({
+    message: "Validating query connection",
+    status: 200
+  });
+}
